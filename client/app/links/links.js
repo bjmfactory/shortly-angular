dbcf5b47868856131b6f6694f723629ca52cb713
MKS-Student <mks.student@hackreactor.com>
2015-04-03 17:46:46 -0500

    adds link controller functionality. 12 tests passing.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,5 +3,15 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
   $scope.data = {};
 
-  $scope.getLinks = function(){};
+  $scope.getLinks = function(){
+  	Links.getLinks($scope.data)
+  	  .then(function(data){
+  	  	$scope.data['links'] = data;
+  	  	console.log("links data is ", data);
+  	  })
+  	  .catch(function(error){
+  	  	console.error(error);
+  	  });
+  };	
+  $scope.getLinks();
 });
